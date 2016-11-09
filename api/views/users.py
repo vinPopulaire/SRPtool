57b36d3e9a92be2f0fe5c0b85ad29ed651fef8cb
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2016-11-09 17:41:26 +0200

    add view for User

--- api/views/users.py
+++ api/views/users.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from api.models import User
+from api.serializers import UserSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
