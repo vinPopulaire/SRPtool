1699e2bad45d643b782102cac6a00646c8da017e
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2016-11-09 17:13:16 +0200

    make User usernames unique

--- api/models/users.py
+++ api/models/users.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class User(models.Model):
-    username = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     email = models.EmailField()
