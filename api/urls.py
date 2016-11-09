a33e2bd03fc432f36307ee6919e2b5a2836348c3
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2016-11-09 17:41:50 +0200

    create urls for User views

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
+
+urlpatterns = [
+    url(r'^api/$', views.UserList.as_view()),
+    url(r'^api/(?P<pk>[0-9]+)/$', views.UserDetail.as_view())
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
