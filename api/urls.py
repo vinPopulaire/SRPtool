3620a26103c72acc2aa9a7b335ab5dedecddf3c4
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2016-11-11 13:36:42 +0200

    add names in urls

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    url(r'^api/$', views.UserList.as_view()),
-    url(r'^api/(?P<username>[a-z0-9]+)/$', views.UserDetail.as_view())
+    url(r'^api/$',
+        views.UserList.as_view(),
+        name='user_list'),
+    url(r'^api/(?P<username>[a-z0-9]+)/$',
+        views.UserDetail.as_view(),
+        name='user_detail')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
