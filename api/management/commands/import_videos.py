2c11b5179457e80f4f6563b83ee09d51c5c28fb2
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-01-17 17:33:20 +0200

    create function to import videos in database from url

---
+++ api/management/commands/import_videos.py
@@ -0,0 +1,63 @@
+from urllib.request import urlopen
+
+import xmltodict
+import json
+from pathlib import Path
+
+from django.core.management.base import BaseCommand
+from api.models import Video
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        my_file = Path("api/management/commands/data_files/videos.json")
+
+        if my_file.is_file():
+            print("Parsing content from videos.json file")
+
+        else:
+            print("Creating videos.json file")
+
+            uri = 'http://a1.noterik.com:8081/smithers2/domain/espace/user/luce/video'
+
+            file = urlopen(uri)
+            data = file.read()
+            file.close()
+
+            json_data = xmltodict.parse(data)
+
+            with my_file.open('w+') as outfile:
+                json.dump(json_data, outfile)
+
+
+        with my_file.open('r') as data_file:
+            data = json.load(data_file)
+
+
+        for i in data['fsxml']['video']:
+            euscreen = i.get('@id')
+
+            if not Video.objects.filter(euscreen=euscreen).exists():
+
+                genre = i['properties'].get('genre')
+                topic = i['properties'].get('topic')
+                title = i['properties'].get('TitleSet_TitleSetInEnglish_title')
+                geographical_coverage = i['properties'].get('SpatioTemporalInformation_SpatialInformation_GeographicalCoverage')
+                summary = i['properties'].get('summaryInEnglish')
+                thesaurus_terms = i['properties'].get('ThesaurusTerm')
+
+                geographical_coverage = geographical_coverage if geographical_coverage != None else 'Unknown'
+                thesaurus_terms = thesaurus_terms if thesaurus_terms != None else "Unknown"
+
+                video = Video(
+                        euscreen=euscreen,
+                        genre=genre,
+                        topic=topic,
+                        title=title,
+                        geographical_coverage=geographical_coverage,
+                        thesaurus_terms=thesaurus_terms,
+                        summary=summary
+                    )
+                video.save()
+
+        print("Done with importing videos")
