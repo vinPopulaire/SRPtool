346f333bfe360544daa075dac9e7678f727a94c7
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-01-19 12:48:24 +0200

    PRETTIFICATION video serializer fields value

--- api/serializers/videos.py
+++ api/serializers/videos.py
@@ -5,4 +5,4 @@ from api.models import Video
 class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
-        fields = ('euscreen','genre','topic','title','geographical_coverage','thesaurus_terms','summary')
+        fields = (['euscreen','genre','topic','title','geographical_coverage','thesaurus_terms','summary'])
