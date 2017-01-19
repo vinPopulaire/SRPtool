ad200a57d479779a6d9ceefe832d7f3b24f966cf
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-01-19 12:35:11 +0200

    create actions serializer

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,3 +1,4 @@
 from .demographics import GenderSerializer, AgeSerializer, EducationSerializer, OccupationSerializer, CountrySerializer
 from .users import UserSerializer
 from .videos import VideoSerializer
+from .actions import ActionSerializer
