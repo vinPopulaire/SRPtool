d6a7856180e5516d18f1a9c591be762be625dea7
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-01-26 14:08:02 +0200

    create enrichment content score table

--- api/models/enrichments.py
+++ api/models/enrichments.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .terms import Term
 
 class Enrichment(models.Model):
     enrichment_id = models.CharField(unique=True, max_length=50)
@@ -18,3 +19,15 @@ class Enrichment(models.Model):
     def __str__(self):
         return self.enrichment_id
 
+
+class EnrichmentContentScore(models.Model):
+    enrichment = models.ForeignKey(Enrichment, on_delete=models.CASCADE)
+    term = models.ForeignKey(Term, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=6, decimal_places=3)
+
+    class Meta:
+        verbose_name = "EnrichmentContentScore"
+        verbose_name_plural = "EnrichmentContentScores"
+
+    def __str__(self):
+        return self.score
