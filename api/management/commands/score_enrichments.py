ca74e2b322ef0505dcb64520a5186a6f03d305b8
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-02-22 18:07:55 +0200

    score enrichments on terms

---
+++ api/management/commands/score_enrichments.py
@@ -0,0 +1,55 @@
+from django.core.management.base import BaseCommand
+from api.models import Enrichment, EnrichmentContentScore
+from api.models import Term
+
+from pathlib import Path
+from os import listdir
+from os.path import isfile, join
+import sys
+
+import gensim
+from gensim.models import Doc2Vec
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        model_path = "./api/management/commands/data_files/doc2vec.model"
+        my_file = Path(model_path)
+
+        if my_file.is_file():
+            print("Using model from file")
+
+        else:
+            sys.exit()
+
+        model = Doc2Vec.load(model_path)
+
+        terms_list = Term.objects.all()
+        enrichments_list = Enrichment.objects.all()
+
+        for term in terms_list:
+
+            print("Scoring for term: ", term.long_name)
+
+            tokens_term = gensim.utils.simple_preprocess(term.long_name)
+
+            for enrichment in enrichments_list:
+                tokens_enrichment = gensim.utils.simple_preprocess(enrichment.description)
+                similarity = model.docvecs.similarity_unseen_docs(model,tokens_term,tokens_enrichment)
+
+                similarity = similarity if similarity>0 else 0
+
+                enrichment_score = EnrichmentContentScore.objects.filter(enrichment_id=enrichment.id).filter(term_id=term.id)
+
+                if enrichment_score.exists():
+                    enrichment_score.update(score=similarity)
+                else:
+                    enrichment_score = EnrichmentContentScore(
+                            enrichment_id=enrichment.id,
+                            term_id=term.id,
+                            score=similarity
+                            )
+                    enrichment_score.save()
+
+        print("Done scoring enrichments!")
