b871edfccda0dd4784a50792e56561eb61c90e6a
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-09-06 13:44:26 +0300

    fix urls references and trailing slash

--- gui/urls.py
+++ gui/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 
     url(r'^$', views.home, name="home"),
-    url(r'^terms$', views.terms, name="terms"),
-    url(r'^about$', views.about, name="about"),
-    url(r'^signup$', views.signup, name="signup"),
+    url(r'^terms/$', views.terms, name="terms"),
+    url(r'^about/$', views.about, name="about"),
+    url(r'^signup/$', views.signup, name="signup"),
     url(r'^login/$', auth_views.login, {'template_name': 'gui/login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout')
 
