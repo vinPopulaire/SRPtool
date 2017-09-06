3aea8711708f00a3bb088ea60694716ab2c69a45
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-09-06 17:17:53 +0300

    add fields to signup

--- gui/forms.py
+++ gui/forms.py
@@ -7,15 +7,14 @@ class SignupForm(UserCreationForm):
     input_class = 'form-control'
 
     email = forms.EmailField(max_length=254, help_text='Required. Provide a valid email address',
-                             widget=forms.TextInput(attrs={'placeholder': 'Your Email',
-                                                           'class': input_class}))
+                             label="Email")
 
     class Meta:
         input_class = 'form-control'
 
         model = AuthUser
 
-        fields = ('username', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
     def __init__(self, *args, **kwargs):
         input_class = 'form-control'
@@ -23,4 +22,5 @@ class SignupForm(UserCreationForm):
         for field in self.base_fields.values():
             field.widget.attrs["placeholder"] = field.label
             field.widget.attrs["class"] = input_class
+            field.widget.attrs["required"] = 'required'
         super(UserCreationForm, self).__init__(*args, **kwargs)
