abb0677b29a792651f62c21d2665c83c766a1673
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-09-07 16:13:56 +0300

    make forms choices return id instead of text for demographics

--- gui/forms.py
+++ gui/forms.py
@@ -21,23 +21,23 @@ class SignupForm(UserCreationForm):
 
         # dynamically get form fields
         response = requests.get("http://localhost:8000/api/age")
-        ages = list(d["age"] for d in response.json())
-        self.base_fields["age"] = forms.ChoiceField(choices=[("", "Select your age")] + list(zip(ages, ages)),
+        ages = list(zip(list(d["id"] for d in response.json()), list(d["age"] for d in response.json())))
+        self.base_fields["age"] = forms.ChoiceField(choices=[("", "Select your age")] + ages,
                                                     label="Age")
 
         response = requests.get("http://localhost:8000/api/country")
-        countries = list(d["country"] for d in response.json())
-        self.base_fields["country"] = forms.ChoiceField(choices=[("", "Select your country")] + list(zip(countries, countries)),
+        countries = list(zip(list(d["id"] for d in response.json()), list(d["country"] for d in response.json())))
+        self.base_fields["country"] = forms.ChoiceField(choices=[("", "Select your country")] + countries,
                                                         label="Country")
 
         response = requests.get("http://localhost:8000/api/education")
-        educations = list(d["education"] for d in response.json())
-        self.base_fields["education"] = forms.ChoiceField(choices=[("", "Select your education")] + list(zip(educations, educations)),
+        educations = list(zip(list(d["id"] for d in response.json()), list(d["education"] for d in response.json())))
+        self.base_fields["education"] = forms.ChoiceField(choices=[("", "Select your education")] + educations,
                                                           label="Education")
 
         response = requests.get("http://localhost:8000/api/occupation")
-        occupations = list(d["occupation"] for d in response.json())
-        self.base_fields["occupation"] = forms.ChoiceField(choices=[("", "Select your occupation")] + list(zip(occupations, occupations)),
+        occupations = list(zip(list(d["id"] for d in response.json()), list(d["occupation"] for d in response.json())))
+        self.base_fields["occupation"] = forms.ChoiceField(choices=[("", "Select your occupation")] + occupations,
                                                            label="Occupation")
 
         input_class = 'form-control'
