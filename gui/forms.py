9c78d19f16621a1d1e194a17bcc6326f45cf8a63
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-09-07 13:19:16 +0300

    dynamically populate signup form

--- gui/forms.py
+++ gui/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User as AuthUser
+import requests
 
 
 class SignupForm(UserCreationForm):
@@ -17,6 +18,28 @@ class SignupForm(UserCreationForm):
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
 
     def __init__(self, *args, **kwargs):
+
+        # dynamically get form fields
+        response = requests.get("http://localhost:8000/api/age")
+        ages = list(d["age"] for d in response.json())
+        self.base_fields["age"] = forms.ChoiceField(choices=[("", "Select your age")] + list(zip(ages, ages)),
+                                                    label="Age")
+
+        response = requests.get("http://localhost:8000/api/country")
+        countries = list(d["country"] for d in response.json())
+        self.base_fields["country"] = forms.ChoiceField(choices=[("", "Select your country")] + list(zip(countries, countries)),
+                                                        label="Country")
+
+        response = requests.get("http://localhost:8000/api/education")
+        educations = list(d["education"] for d in response.json())
+        self.base_fields["education"] = forms.ChoiceField(choices=[("", "Select your education")] + list(zip(educations, educations)),
+                                                          label="Education")
+
+        response = requests.get("http://localhost:8000/api/occupation")
+        occupations = list(d["occupation"] for d in response.json())
+        self.base_fields["occupation"] = forms.ChoiceField(choices=[("", "Select your occupation")] + list(zip(occupations, occupations)),
+                                                           label="Occupation")
+
         input_class = 'form-control'
         
         for field in self.base_fields.values():
