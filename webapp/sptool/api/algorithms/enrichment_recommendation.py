a14a16af69bc0a81fe0f17879a600e914d664e70
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2017-10-23 17:07:51 +0300

    speed up enrichment recommendations

--- webapp/sptool/api/algorithms/enrichment_recommendation.py
+++ webapp/sptool/api/algorithms/enrichment_recommendation.py
@@ -13,11 +13,35 @@ def enrichments_recommendation(user_vector, video_id):
     if user_vector == [0] * len(user_vector):
         user_vector = [0.1] * len(user_vector)
 
-    enrichments = VideoEnrichments.objects.filter(video_id=video_id)
+    # what enrichments are present in the video
+    video_enrichments = VideoEnrichments.objects.filter(video_id=video_id)
+
+    # information about the enrichments themselves
+    enrichments = Enrichment.objects.filter(videoenrichments__in=video_enrichments)
+
+    # score of the present enrichments
+    enrichment_content_scores = EnrichmentContentScore.objects.filter(enrichment_id__in=enrichments)
+
+    # Force evaluate queryset for fast .score
+    len(enrichment_content_scores)
+
+    num_terms = Term.objects.count()
+
+    enrichment_vectors = {}
+    for item in enrichment_content_scores:
+
+        # term ids start from 1 while term vector indexing starts from 0
+        term_index = item.term_id - 1
+
+        if item.enrichment_id in enrichment_vectors:
+            enrichment_vectors[item.enrichment_id][term_index] = float(item.score)
+        else:
+            enrichment_vectors[item.enrichment_id] = [None] * num_terms
+            enrichment_vectors[item.enrichment_id][term_index] = float(item.score)
 
     # keep enrichments that appear on the same time in a dictionary of lists
     dict_enrichments = defaultdict(list)
-    for enrichment in enrichments:
+    for enrichment in video_enrichments:
         time = enrichment.time
         dict_enrichments[time].append(enrichment)
 
@@ -26,7 +50,7 @@ def enrichments_recommendation(user_vector, video_id):
 
     # check if enrichments that appear on the same time are on the same annotation
     recommended_enrichments = []
-    for key, candidate_enrichments in dict_enrichments.items():
+    for time, candidate_enrichments in dict_enrichments.items():
 
         # keep track if enrichment is already processed (if it was on the same object to an other one)
         checked_enrichments = set()
@@ -36,10 +60,10 @@ def enrichments_recommendation(user_vector, video_id):
             if enrichment_id in checked_enrichments:
                 continue
 
-            similarity = enrichment_similarity(user_vector, enrichment_id)
+            similarity = enrichment_similarity(user_vector, enrichment_vectors[enrichment_id])
 
             # ATTENTION if tuple is changed, the index in the lambda function for sorting the tuples must also change
-            simultaneous_enrichments = [tuple([key, Enrichment.objects.get(id=enrichment_id).enrichment_id, similarity])]
+            simultaneous_enrichments = [tuple([time, Enrichment.objects.get(id=enrichment_id).enrichment_id, similarity])]
             checked_enrichments.add(enrichment_id)
 
             for tmp_enrichment in candidate_enrichments:
@@ -53,10 +77,10 @@ def enrichments_recommendation(user_vector, video_id):
                         and enrichment.x_min == tmp_enrichment.x_min \
                         and enrichment.y_min == tmp_enrichment.y_min:
 
-                    similarity = enrichment_similarity(user_vector, tmp_enrichment_id)
+                    similarity = enrichment_similarity(user_vector, enrichment_vectors[tmp_enrichment_id])
 
                     simultaneous_enrichments.append(
-                        tuple([key, Enrichment.objects.get(id=tmp_enrichment_id).enrichment_id, similarity]))
+                        tuple([time, Enrichment.objects.get(id=tmp_enrichment_id).enrichment_id, similarity]))
                     checked_enrichments.add(tmp_enrichment_id)
 
             # ATTENTION if tuple is changed, the index in the lambda function for sorting the tuples must also change
@@ -66,18 +90,7 @@ def enrichments_recommendation(user_vector, video_id):
     return recommended_enrichments
 
 
-def enrichment_similarity(user_vector, enrichment_id):
-
-    num_terms = Term.objects.count()
-
-    enrichment_content_score = EnrichmentContentScore.objects.filter(enrichment=enrichment_id).order_by('term_id')
-
-    # Force evaluate queryset for fast .score
-    len(enrichment_content_score)
-    enrichment_vector = [None] * num_terms
-
-    for jj in range(num_terms):
-        enrichment_vector[jj] = float(enrichment_content_score[jj].score)
+def enrichment_similarity(user_vector, enrichment_vector):
 
     similarity = cosine_similarity(user_vector, enrichment_vector)
 
