2ef0b60d4f58df82812fdce73940a2a2ba6785c7
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2018-02-09 11:08:19 +0200

    add euclidean similarity metric

--- webapp/sptool/api/algorithms/cosine_similarity.py
+++ webapp/sptool/api/algorithms/cosine_similarity.py
@@ -20,3 +20,24 @@ def cosine_similarity(vector_1, vector_2):
     similarity = numerator / (math.sqrt(sum_a)*math.sqrt(sum_b))
 
     return similarity
+
+def euclidean_similarity(vector_1, vector_2):
+
+    similarity = 1/(1 + euclidean_distance(vector_1, vector_2))
+
+    return similarity
+
+def euclidean_distance(vector_1, vector_2):
+
+    num_terms = len(vector_1)
+    sum_a = 0
+
+    for ii in range(num_terms):
+        score_1 = vector_1[ii]
+        score_2 = vector_2[ii]
+
+        sum_a += math.pow((score_1 - score_2),2)
+
+    distance  = math.sqrt(sum_a)
+
+    return distance
