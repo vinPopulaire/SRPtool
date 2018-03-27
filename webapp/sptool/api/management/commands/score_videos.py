2bb4ff18d29490ddf19c6d5e8322a743c47ddf9f
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2018-03-27 16:03:00 +0300

    change bulk scoring of videos to use word2Vec

--- webapp/sptool/api/management/commands/score_videos.py
+++ webapp/sptool/api/management/commands/score_videos.py
@@ -3,11 +3,9 @@ from ...models import Video, VideoContentScore
 from ...models import Term
 
 from pathlib import Path
-from os import listdir
-import sys
 
 import gensim
-from gensim.models import Doc2Vec
+from gensim.models import KeyedVectors
 
 
 class LabeledLineSentence(object):
@@ -23,85 +21,66 @@ class LabeledLineSentence(object):
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        model_path = "./srv/sptool/api/management/commands/data_files/doc2vec.model"
+        model_path = "/srv/sptool/api/management/commands/data_files/word2vec.txt"
         my_file = Path(model_path)
 
         if my_file.is_file():
-            print("Using model from file")
-
+            pass
         else:
-            print("Creating model")
+            raise FileNotFoundError("Model for scoring not found")
 
-            docLabels = []
-            docLabels = [f for f in listdir('./srv/sptool/api/management/commands/data_files/video_txts') if
-                         f.endswith('.txt')]
-
-            if docLabels == []:
-                print("Error! There are no txt files")
-                sys.exit()
-
-            data = []
-
-            for doc in docLabels:
-                with open("./srv/sptool/api/management/commands/data_files/video_txts/" + doc, 'r') as fout:
-                    data.append(fout.read())
-
-            # TODO Data has \n inside. Check if it matters
-            # TODO remove stopwords
-
-            it = LabeledLineSentence(data, docLabels)
-
-            # TODO check other parameters
-            model = Doc2Vec(dm=0, size=300, window=5, negative=5, min_count=0, workers=4)
-
-            model.build_vocab(it)
-
-            # TODO check if shuffling data first helps (as suggested on gensim github)
-            # TODO why does it have to model.train calls
-            for epoch in range(10):
-                model.train(it)
-                model.alpha -= 0.002
-                model.min_alpha = model.alpha
-                model.train(it)
-
-            # Import google_news word2vec on our model
-            # TODO check if the word2vec from google news helps
-            model.intersect_word2vec_format(
-                './srv/sptool/api/management/commands/data_files/GoogleNews-vectors-negative300.bin', binary=True)
-
-            model.save(model_path)
-
-        model = Doc2Vec.load(model_path)
+        print("Loading model")
+        model = KeyedVectors.load_word2vec_format(model_path)
+        print("Model loaded")
 
         terms_list = Term.objects.all()
         videos_list = Video.objects.all()
 
-        num_of_videos = len(videos_list)
+        num_videos = 1
 
-        for term in terms_list:
+        for video in videos_list:
 
-            print("Scoring for term: ", term.long_name)
+            data = video.title + " " + \
+                   video.topic + " " + \
+                   video.genre + " " + \
+                   video.thesaurus_terms + " " + \
+                   video.tags + " " + \
+                   video.annotations
 
-            tokens = gensim.utils.simple_preprocess(term.long_name)
-            new_doc_vec = model.infer_vector(tokens)
+            video_tokens = gensim.utils.simple_preprocess(data)
 
-            # TODO check if there exists another api call that doesn't rank (for efficiency)
-            similarities = model.docvecs.most_similar([new_doc_vec], topn=num_of_videos)
-            for item in similarities:
-                video_euscreen = item[0][:-4]
-                video = videos_list.get(euscreen=video_euscreen)
+            for term in terms_list:
 
-                similarity = item[1] if item[1] > 0 else 0
+                # simple_preprocess returns a list with all tokens (just one in our case)
+                term_token = gensim.utils.simple_preprocess(term.term)[0]
+
+                # we will keep maximum similarity because it seems to provide clearer results
+                # average similarity can also be considered
+                max_similarity = 0
+                for video_token in video_tokens:
+                    # calculate similarity. If word does not exist in model, skip it
+                    try:
+                        similarity = model.wv.similarity(video_token, term_token)
+                    except KeyError:
+                        continue
+                    if max_similarity < similarity:
+                        max_similarity = similarity
+                    max_similarity = similarity if max_similarity < similarity else max_similarity
+                max_similarity = max_similarity if max_similarity > 0 else 0
 
                 video_score = VideoContentScore.objects.filter(video_id=video.id).filter(term_id=term.id)
                 if video_score.exists():
-                    video_score.update(score=similarity)
+                    video_score.update(score=max_similarity)
                 else:
                     scoring = VideoContentScore(
                         video_id=video.id,
                         term_id=term.id,
-                        score=similarity
+                        score=max_similarity
                     )
                     scoring.save()
+
+            num_videos += 1
+            if num_videos%50 == 0:
+                print("%d videos scored!" % num_videos)
 
         print("Done scoring!")
