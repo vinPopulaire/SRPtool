5cb0eeb2961db163c47d1fbfdf1adcec5669a501
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2018-04-03 16:20:48 +0300

    authentication is performed silently from SSO

--- webapp/sptool/gui/auth0backend.py
+++ webapp/sptool/gui/auth0backend.py
@@ -1,5 +1,8 @@
 import requests
 from social_core.backends.oauth import BaseOAuth2
+from social_core.utils import handle_http_errors
+from social_core.exceptions import AuthFailed
+from django.shortcuts import redirect
 
 
 class Auth0(BaseOAuth2):
@@ -10,6 +13,39 @@ class Auth0(BaseOAuth2):
     EXTRA_DATA = [
         ('picture', 'picture')
     ]
+
+    @handle_http_errors
+    def auth_complete(self, *args, **kwargs):
+        """Completes login process, must return user instance"""
+        try:
+            self.process_error(self.data)
+            state = self.validate_state()
+
+            response = self.request_access_token(
+                self.access_token_url(),
+                data=self.auth_complete_params(state),
+                headers=self.auth_headers(),
+                auth=self.auth_complete_credentials(),
+                method=self.ACCESS_TOKEN_METHOD
+            )
+            self.process_error(response)
+        except AuthFailed:
+            return redirect('/')
+        return self.do_auth(response['access_token'], response=response,
+                            *args, **kwargs)
+
+    def auth_params(self, state=None):
+        client_id, client_secret = self.get_key_and_secret()
+        params = {
+            'client_id': client_id,
+            'redirect_uri': self.get_redirect_uri(state),
+            'prompt': 'none'
+        }
+        if self.STATE_PARAMETER and state:
+            params['state'] = state
+        if self.RESPONSE_TYPE:
+            params['response_type'] = self.RESPONSE_TYPE
+        return params
 
     def authorization_url(self):
         """Return the authorization endpoint."""
