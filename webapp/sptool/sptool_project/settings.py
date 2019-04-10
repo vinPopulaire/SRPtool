400db2d68d70f161e08494fb1bb56c50a0d41ae1
Giorgos Mitsis <gmitsis@netmode.ntua.gr>
2019-04-10 15:23:30 +0300

    add allowed hosts to settings

--- webapp/sptool/sptool_project/settings.py
+++ webapp/sptool/sptool_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('DJANGO_ALLOWED_HOSTS')]
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login/'
